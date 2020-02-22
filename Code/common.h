fd52d4766dd07a70b842fcaf70a295971456ff2f
nju-xy <171240536@smail.nju.edu.cn>
2020-02-22 20:59:38 +0800

    完成词法分析中各词素的匹配

---
+++ Code/common.h
@@ -0,0 +1,6 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <stdio.h>
+
+#endif
---
+++ Code/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// #include <common.h>
+
+extern FILE* yyin;
+int main(int argc, char ** argv){
+    if(argc > 1) {
+        if(!(yyin = fopen(argv[1], "r"))) { // 第一个参数为要打开的文件名
+            perror(argv[1]); // 如果打开文件失败，报错
+            return 1;
+        }
+    }
+    while(yylex() != 0); //打开文件成功，就调用yylex()进行词法分析
+    return 0;
+}
