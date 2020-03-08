103afa1687be9603cae9555d03b85351e2a3694a
nju-xy <171240536@smail.nju.edu.cn>
2020-03-08 11:45:28 +0800

    词法阶段加了一些错误int和错误float的识别

--- Code/main.c
+++ Code/main.c
@@ -2,6 +2,7 @@
 // #include <common.h>
 
 extern FILE* yyin;
+extern int yylex();
 int main(int argc, char ** argv){
     if(argc > 1) {
         if(!(yyin = fopen(argv[1], "r"))) { // 第一个参数为要打开的文件名
